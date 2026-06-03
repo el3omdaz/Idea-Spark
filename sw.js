060e681c8c016c3dfa8613e2b89cee8fb42d1161
el3omdaz <70915479+el3omdaz@users.noreply.github.com>
2026-06-04 01:00:43 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ideaspark-v3';
+const CACHE = 'ideaspark-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
